0a42bd7dd65668183a91e3535fe0c3586e7fb48c
gouravroy2626 <gouravroy2626@gmail.com>
2025-11-11 10:40:50 +0530

    Vehicle Forms Skeleton

--- New-Car-Loan/src/app/app.ts
+++ New-Car-Loan/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { VehicleForm } from './features/vehicle-details/components/vehicle-form/vehicle-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,VehicleForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ New-Car-Loan/src/app/features/vehicle-details/components/vehicle-form/vehicle-form.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-form',
+  imports: [],
+  templateUrl: './vehicle-form.html',
+  styleUrls: ['./vehicle-form.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class VehicleForm {
+
+}
