6ca590816a23f08f2ef3cd106a5e5b86503911fc
manoj-kumbhar <kumbharmanoj244@gmail.com>
2025-11-11 10:41:16 +0530

    Navbar Fixed

--- New-Car-Loan/src/app/app.routes.ts
+++ New-Car-Loan/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+// import { Home } from './features/home/components/home/home';
+// import { PersonalForm } from './features/personal-details/components/personal-form/personal-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {
+    //     path:"",
+    //     component:Home
+    // },
+    // {
+    //     path:"personal-details",
+    //     component:PersonalForm
+    // }
+];
--- New-Car-Loan/src/app/shared/components/navbar/navbar.ts
+++ New-Car-Loan/src/app/shared/components/navbar/navbar.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  private currentRoute: string = '';
 
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.currentRoute = event.url;
+      });
+  }
+
+  showBackButton(): boolean {
+    return this.currentRoute !== '/' && this.currentRoute !== '';
+  }
+
+  goBack(): void {
+    this.router.navigate(['/']);
+  }
 }
